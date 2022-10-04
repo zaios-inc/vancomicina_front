be8edbc8d5cc1072f9dc01c0e184387d2e81e115
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-10-04 16:57:20 -0500

    adding selecto component and select, patient, antibiotic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import SignUp from "./components/signUp";
 import Login from "./components/login";
+import Select from "./components/Select";
+import Patient from "./components/Patient";
+import Antibiotic from "./components/Antibiotic";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/register" element={<SignUp/>}/>
+        <Route exact path="/select" element={<Select />}/>
+        <Route exact path="/patient" element={<Patient />}/>
+        <Route exact path="/antibiotic" element={<Antibiotic />}/>
       </Routes>
     </Router>
   );

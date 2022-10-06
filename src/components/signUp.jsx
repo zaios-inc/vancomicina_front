c29ec7ee895e5f113cb6438c572e32054e965dd4
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-10-06 11:34:10 -0500

    [ADDED] set timer and alert to check if the user was created

--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Error from './error';
 import axios from 'axios';
+const Swal = require('sweetalert2')
 
 function SignUp() {
 
@@ -32,10 +33,25 @@ function SignUp() {
       name: name,
       password: password
     }
-
+    let timerInterval 
+    Swal.fire({
+        icon: 'info',
+        title: 'Creando tu usuario...',
+        showConfirmButton: false,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft()
+          }, 100)
+      }
+    })
     axios.post('/user/register', newUser)
-    .then(res =>{
-      alert(res.data)
+    .then(res => {
+      Swal.fire({
+          title:'Tu Usuario se creo Correctamente!',
+          icon: 'success'
+      })
     })
     .then(err => {console.log(err)})
 

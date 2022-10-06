13e78afd926887caa77f0695652fe7b04f7e780f
DevPacho <frankjose2004@gmail.com>
2022-10-06 18:19:37 -0500

    [UPDATED] general redesign of frontend and sidebar on all pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import Select from "./components/Select";
 import PatientRegistry from "./components/patientRegistry"
 import Antibiotic from "./components/Antibiotic";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/sidebar";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/register" element={<SignUp/>}/>
-        <Route exact path="/select" element={<Select />}/>
-        <Route exact path="/patient" element={<PatientRegistry />}/>
-        <Route exact path="/antibiotic" element={<Antibiotic />}/>
-      </Routes>
-    </Router>
+      <Sidebar />
+    <Routes>
+      <Route exact path="/login" element={<Login/>}/>
+      <Route exact path="/register" element={<SignUp/>}/>
+      <Route exact path="/select" element={<Select />}/>
+      <Route exact path="/patient" element={<PatientRegistry />}/>
+      <Route exact path="/antibiotic" element={<Antibiotic />}/>
+    </Routes>
+  </Router>
   );
 }
 
--- src/components/patientRegistry.jsx
+++ src/components/patientRegistry.jsx
@@ -1,128 +1,79 @@
+import { Card, CardBody, Input, CardHeader, Typography,Button, CardFooter, Select, Option } from '@material-tailwind/react';
 import React from 'react'
+import Sidebar from './sidebar';
 
 function PatientRegistry() {
   return (
-    <div class="min-h-screen py-40" style={{ background: 'linear-gradient(to right, #261089, #533eab, #9190ff, #6674ff, #261089)' }}>
-      <div class="flex flex-col md:w-1/4 pr-4 pl-4 text-center items-center text-white">
-          <h3 class="text-3xl font-bold">Bienvenido</h3>
-          <p>Pilas porque no sé que poner aquí</p><br />
-          <img
-            src=""
-            alt=""
-            >
-          </img>
-          <button
-            type="button"
-            class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-2xl shadow-md hover:bg-blue-700 pulse 1s; focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out mb-3"
-            style={{ background: 'linear-gradient(to right, #00dfc3, #533eab, #9190ff, #6674ff, #261089)' }}
-          >
-            Atrás
-          </button>
+    <div className='flex min-h-screen bg-purple-50'>
+
+      <div className='w-1/6'>
+        <Sidebar />
       </div>
-      <div class="container mx-auto">
-        <div class="bg-white w-8/12 rounded-tl-full rounded-bl-full mx-auto shadow-2xl shadow-[#261089] overflow-hidden"> { /* Main div */}
-          <div class="block">
-            <h3 class="text-center text-3xl mt-5 font-bold pb-5">Registro de pacientes</h3>
-          </div>
 
-          <form action="#" class="flex justify-center">
-            <div class="">
-              <div class="py-3 px-4">
-                <input
-                  type="text"
-                  class="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  id="exampleFormControlInput1"
-                  placeholder="Nombre"
-                />
-              </div>
-              <div class="py-3 px-4">
-                <input
-                  type="email"
-                  class="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  id="exampleFormControlInput1"
-                  placeholder="Identificación"
-                  maxlength="3"
-                />
-              </div>
-              <div class="py-3 px-4">
-                <input
-                  type="number"
-                  class="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  id="exampleFormControlInput1"
-                  placeholder="Altura"
-                />
-              </div>
-              <div class="py-3 px-4">
-                <input
-                  type="text"
-                  class="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  id="exampleFormControlInput1"
-                  placeholder="Creatinina"
-                />
-              </div>
-            </div>
+        <div className='pl-60 py-32'>
+        <Card className="w-96 shadow-2xl shadow-[#9190ff]">
+      <CardHeader
+        variant="gradient"
+        className="grid h-20 place-items-center shadow-lg shadow-[#9190ff]"
+        color='purple'
+      >
+        <Typography variant="h3" color="white">
+          Datos del paciente
+        </Typography>
+      </CardHeader>
+      <CardBody className="flex flex-col gap-4">
+        <Input type="number" label="Altura" variant="standard" size="lg" color='purple'/>
+        <Input type="number" label="Peso" variant="standard" size="lg" color='purple'/>
+        <Input type="number" label="Creatinina" variant="standard" size="lg" color='purple' />
+        <div class=" pt-5">
+          <label class="pr-2">
+            <input
+              type="radio"
+              name="genero"
+              value="Hombre"
+            />
+            <span> Hombre </span>
+          </label>
 
-
-            <div>
-              <div class="py-3 px-4">
-                <input
-                  type="text"
-                  class="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  id="exampleFormControlInput1"
-                  placeholder="Apellido"
-                />
-              </div>
-              <div class="py-3 px-4">
-                <input
-                  type="text"
-                  class="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  id="exampleFormControlInput1"
-                  placeholder="Edad"
-                />
-              </div>
-              <div class="py-3 px-4">
-                <input
-                  type="text"
-                  class="form-control block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  id="exampleFormControlInput1"
-                  placeholder="Peso"
-                />
-              </div>
-              <div class="px-4 pt-5">
-                <label class="pr-2">
-                  <input
-                    type="radio"
-                    name="genero"
-                    value="Hombre"
-                  />
-                  <span> Hombre </span>
-                </label>
-
-                <label>
-                  <input
-                    type="radio"
-                    name="genero"
-                    value="Mujer"
-                  />
-                  <span> Mujer </span>
-                </label>
-              </div>
-
-              <div class="mt-10">
-              <button
-                type="button"
-                class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-2xl shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out mb-6 w-full"
-                style={{ background: 'linear-gradient(to right, #00dfc3, #533eab, #9190ff, #6674ff, #261089)' }}
-              >
-                Registrar
-              </button>
-              </div>
-
-            </div>
-          </form>
-
+          <label>
+            <input
+              type="radio"
+              name="genero"
+              value="Mujer"
+            />
+            <span> Mujer </span>
+          </label>
         </div>
+
+        <div className="-ml-2.5">
+          Poner mensaje de error aquí
+        </div>
+      </CardBody>
+      <CardFooter className="pt-0">
+
+      <div>
+        <Button variant="gradient" fullWidth color='purple'>
+          Calcular
+        </Button>
       </div>
+
+      <div className='pt-3'>
+        <Button variant="gradient" fullWidth color='purple'>
+          Calcular
+        </Button>
+      </div>
+
+      </CardFooter>
+    </Card>
+        </div>
+
+      <div className='py-36 pl-10'>
+        <input type="text" list="antibiotics" className='rounded-lg w-60 shadow-md shadow-[#9190ff]'/>
+          <datalist id="antibiotics">
+            <option>The best capstone</option>
+          </datalist>
+      </div>
+
     </div>
   )
 }

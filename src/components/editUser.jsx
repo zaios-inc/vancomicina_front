29141b82a6777bd2e6de8208336559037c9dc475
DevPacho <frankjose2004@gmail.com>
2022-10-23 20:04:58 -0500

    [UPDATED] style redesign of edit user account section

--- src/components/editUser.jsx
+++ src/components/editUser.jsx
@@ -38,119 +38,113 @@ function User() {
       newpassword: newpassword
     }
     axios.post(`${backendString}/user/updateuser`, updateUser)
-    .then(res => {
-      Swal.fire({
-        title: res.data,
-        icon: 'info'
+      .then(res => {
+        Swal.fire({
+          title: res.data,
+          icon: 'info'
+        })
+        navigate('../home')
       })
-      navigate('../home')
-    })
-    .then(err => {console.log(err)})
+      .then(err => { console.log(err) })
   }
 
   function showChecked() {
-    var element = document.getElementById("content");
+    const element = document.getElementById('content');
     setShow(document.getElementById('newpassword'))
     console.log(user)
     if (show.checked === true) {
-      element.className='visible';
+      element.className = 'visible';
     } else {
-      element.className='invisible';
+      element.className = 'invisible';
     }
   }
 
   return (
-    <div className='flex'>
-      <div className='w-full'>
-        <div className='flex min-h-screen'>
+    <div className='flex lg:pl-64'>
+      <Sidebar />
 
-          <div className='w-1/6' >
-            <Sidebar />
-          </div>
-
-          <div className='w-full' style={{ background: 'linear-gradient(to right, #261089, #9190ff, #261089)' }}>
-            <div className='flex justify-center items-center'>
-              <Card className='w-auto h-1/2 shadow-2xl shadow-[#261089] z-10'>
-                <CardHeader
-                  variant='gradient'
-                  className='grid h-20 place-items-center shadow-lg shadow-[#261089] bg-[#261089]'
-                >
-                  <Typography variant='h3' color='white' className='p-5'>
-                    Información de la cuenta
-                  </Typography>
-                </CardHeader>
-                <div className='flex'>
-                  <div className='w-full'>
-                    <CardBody className='flex flex-col gap-4'>
-                      <Input
-                        type='text'
-                        maxLength="30"
-                        label='Nombre:'
-                        variant='standard'
-                        size='lg'
-                        color='deep-purple'
-                        value={name}
-                        onChange={(e) => { (setName(e.target.value)) }}
-                      />
-                      <Input
-                        type='text'
-                        maxLength="30"
-                        label='Correo electrónico:'
-                        variant='standard'
-                        size='lg'
-                        color='deep-purple'
-                        value={email}
-                        onChange={(e) => { (setEmail(e.target.value)) }}
-                      />
-                      <div>
-                        <input
-                          id="newpassword"
-                          type="checkbox"
-                          label='Cambiar contraseña'
-                          value={show}
-                          onClick={showChecked}
-                          required
-                        />
-                        <label className='pl-2'>Cambiar contraseña</label>
-                        <div id='content' className='invisible pt-3'>
-                          <Input
-                            type='text'
-                            maxLength="30"
-                            placeholder='Nueva contraseña:'
-                            variant='standard'
-                            size='lg'
-                            color='deep-purple'
-                            value={newpassword}
-                            onChange={(e) => { (setNewPassword(e.target.value)) }}
-                          />
-                        </div>
-                      </div>
-                    </CardBody>
+      <div
+        className='w-full h-screen flex justify-center items-center'
+        style={{
+          background: 'linear-gradient(to right, #261089, #6674ff, #9190ff, #6674ff, #261089)',
+        }}
+      >
+        <Card className='w-auto h-1/2 shadow-2xl shadow-[#261089] z-10'>
+          <CardHeader
+            variant='gradient'
+            className='grid h-20 place-items-center shadow-lg pl-5 pr-5 shadow-[#261089] bg-[#261089]'
+          >
+            <Typography variant='h3' color='white' className='text-center'>
+              Información de la cuenta
+            </Typography>
+          </CardHeader>
+          <div className='flex'>
+            <div className='w-full'>
+              <CardBody className='flex flex-col gap-4'>
+                <Input
+                  type='text'
+                  maxLength='30'
+                  label='Nombre:'
+                  variant='standard'
+                  size='lg'
+                  color='deep-purple'
+                  value={name}
+                  onChange={(e) => { (setName(e.target.value)) }}
+                />
+                <Input
+                  type='text'
+                  maxLength='30'
+                  label='Correo electrónico:'
+                  variant='standard'
+                  size='lg'
+                  color='deep-purple'
+                  value={email}
+                  onChange={(e) => { (setEmail(e.target.value)) }}
+                />
+                <div>
+                  <input
+                    id='newpassword'
+                    type='checkbox'
+                    label='Cambiar contraseña'
+                    value={show}
+                    onClick={showChecked}
+                    required
+                  />
+                  <label className='pl-2'>Cambiar contraseña</label>
+                  <div id='content' className='invisible pt-3'>
+                    <Input
+                      type='text'
+                      maxLength='30'
+                      placeholder='Nueva contraseña:'
+                      variant='standard'
+                      size='lg'
+                      color='deep-purple'
+                      value={newpassword}
+                      onChange={(e) => { (setNewPassword(e.target.value)) }}
+                    />
                   </div>
                 </div>
-
-                <CardFooter className='pt-0'>
-                  <div>
-                    <Button variant='gradient' fullWidth color='deep-purple' className='z-0'
-                      style={{ background: 'linear-gradient(to right, #261089, #9190ff, #261089)' }}
-                      onClick={editarUsuario}
-                    >
-                      Guardar cambios
-                    </Button>
-                  </div>
-
-                </CardFooter>
-              </Card>
-
-              <ParticlesBackground />
-              
+              </CardBody>
             </div>
           </div>
-        </div>
+
+          <CardFooter className='pt-0'>
+            <div>
+              <Button variant='gradient' fullWidth color='deep-purple' className='z-0'
+                style={{ background: 'linear-gradient(to right, #261089, #9190ff, #261089)' }}
+                onClick={editarUsuario}
+              >
+                Guardar cambios
+              </Button>
+            </div>
+
+          </CardFooter>
+        </Card>
       </div>
 
+      <ParticlesBackground />
     </div>
-  )
+  );
 }
 
 export default User;

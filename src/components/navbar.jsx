f27d551da16bc128688e017d7e76af99890c84f9
AFOP <felipe@ASUSAFOP.localdomain>
2022-11-08 20:05:24 -0500

    update navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -47,8 +47,9 @@ function Navbar() {
                         <NavLink
                             to='/edituser'
                             exact
+                            className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                         >
-                            <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Mi perfil</a>
+                            <P>Mi perfil</P>
                         </NavLink>
                     </li>
                 </ul>

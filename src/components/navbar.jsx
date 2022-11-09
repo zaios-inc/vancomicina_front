944eb5090931fce141ca5e131e3566c9f2df6093
AFOP <felipe@ASUSAFOP.localdomain>
2022-11-08 19:55:29 -0500

    update navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -43,7 +43,12 @@ function Navbar() {
                         <a href={`${backendString}/vancomicina_front/vancomycin`} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Vancomicina</a>
                     </li>
                     <li>
-                        <a href={`${backendString}/vancomicina_front/edituser`} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Mi perfil</a>
+                        <NavLink
+                            to='/edituser'
+                            exact
+                        >
+                            <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Mi perfil</a>
+                        </NavLink>
                     </li>
                 </ul>
             </div>
